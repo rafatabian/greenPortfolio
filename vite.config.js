9bad3487b6a9c69b2d7f9b7201d567489e04d2ec
Rata Fabian <rata.fabian1@gmail.com>
2024-06-01 11:54:17 +0100

    testing

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "https://fabian-rata.com/"
-// })
-
-// normal 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "https://fabian-rata.com/"
 })
+
