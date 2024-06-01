0753233426d250fde42bf1c120e8d89507e60e35
Rata Fabian <rata.fabian1@gmail.com>
2024-06-01 11:22:28 +0100

    normal importing of home and navabr

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This is a simple version of my portflio build with React and vite.
 I'm planning to do upcoming projects so this is just the beginning of the adventure. 
+
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { lazy, Suspense, useRef } from "react"
 import "./App.css"
+import Home from "./components/Home/Home"
+import Navbar from "./components/Navbar/Navbar"
 
 const App = () => {
-  const Home = lazy(() => import("./components/Home/Home"))
-  const Navbar = lazy(() => import("./components/Navbar/Navbar"))
+  // const Home = lazy(() => import("./components/Home/Home"))
+  // const Navbar = lazy(() => import("./components/Navbar/Navbar"))
   const About = lazy(() => import("./components/About/About"))
   const Skills = lazy(() => import("./components/Skills/Skills"))
   const Projects = lazy(() => import("./components/Projects/Projects"))
@@ -16,14 +18,12 @@ const App = () => {
     projects: useRef(null)
   }
 
-
 const scrollToSection = (id) => {
    const sectionRef = sectionRefs[id]
    if(sectionRef && sectionRef.current){
     sectionRef.current.scrollIntoView({behavior: 'smooth'})
    }
 }
-
 
 
   return (
