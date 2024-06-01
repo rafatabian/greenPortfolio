b80b06f7ec1d035ecd8632bd2465ab88391ea3cf
Rata Fabian <rata.fabian1@gmail.com>
2024-06-01 07:00:51 +0100

    finding layout shift

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,9 @@ const scrollToSection = (id) => {
   return (
     <div>
       <Suspense fallback={<h1 id="loading">LOADING...</h1>}>
-        {/* <Navbar scrollToSection={scrollToSection}/> */}
+        <Navbar scrollToSection={scrollToSection}/>
         <section ref={sectionRefs.home} id="home">
-          <Home />
+          {/* <Home /> */}
         </section>
         <section ref={sectionRefs.about} id="about">
           <About scrollToSection={scrollToSection}/>
